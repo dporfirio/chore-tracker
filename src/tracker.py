a366a5a957e4e57a3bc81c7dc26d906758d7ce27
David Porfirio <dporfirio@wisc.edu>
2019-07-07 08:50:51 -0500

    added a timer that listens for new days

--- src/tracker.py
+++ src/tracker.py
@@ -2,6 +2,9 @@
 Author: David Porfirio
 '''
 
+import time
+import datetime
+import _thread as thread
 from reader import *
 
 class Tracker:
@@ -15,3 +18,21 @@ class Tracker:
     def __init__(self, filename):
         reader = Reader()
         self.data = reader.read(filename)
+
+        # begin listening for day changes
+        thread.start_new_thread(self.listen_for_day_change, ("thread_timer",))
+
+    def listen_for_day_change(self, name):
+
+        '''
+        Method: listen_for_day_change
+        Purpose: listen for a new day and initiate the updating of the chore
+            objects
+        '''
+
+        old_day = datetime.datetime.now().day
+        while True:
+            time.sleep(5)
+            now_day = datetime.datetime.now().day
+            is_new_day = (now_day != old_day)
+            old_day = now_day
